6ee1eebbe04da923b4fbada28e32b6643759a788
YC2023 <cgl@88.com>
2023-08-27 17:34:14 +0800

    完成添加管理员和其关联权限

--- app/controller/Auth.php
+++ app/controller/Auth.php
@@ -2,13 +2,23 @@
 declare (strict_types = 1);
 namespace app\controller;
 use app\model\Auth as AuthModel;
+use app\model\Role as RoleModel;
 use think\Request;
+use think\Response;
+
 class Auth
 {
     /**
+     * 提示模板路径
+     * @var string
+     */
+    private $toast = 'public/toast';
+
+
+    /**
      * 显示资源列表
      *
-     * @return \think\Response
+     * @return Response
      */
     public function index()
     {
@@ -41,29 +51,49 @@ class Auth
     /**
      * 显示创建资源表单页.
      *
-     * @return \think\Response
+     * @return Response
      */
     public function create()
     {
-        //
+        return view('create',[
+            'list' => RoleModel::select(),
+
+        ]);
     }
 
     /**
      * 保存新建的资源
      *
-     * @param  \think\Request  $request
-     * @return \think\Response
+     * @param Request $request
+     * @return string|Response
      */
     public function save(Request $request)
     {
-        //
+//        return json($request->param());
+        $data = $request->param();
+
+        //TODO:验证暂时先不搞：管理员名>2..pwd>6..
+
+        //加密密码
+        $data['password'] = sha1($data['password']);
+        //写入数据库 后 返回id
+        $id = AuthModel::create($data)->getData('id');
+
+        //关联保存
+        AuthModel::find($id)->role()->saveAll($data['role']);
+
+        return $id ? view($this->toast,[
+            'infos'         =>       ['恭喜,添加成功'],
+            'url_text'      =>      '回到管理页',
+            'url_path'      =>      url('/auth')
+        ]):'添加失败!';
     }
 
     /**
      * 显示指定的资源
      *
      * @param  int  $id
-     * @return \think\Response
+     * @return Response
      */
     public function read($id)
     {
@@ -74,7 +104,7 @@ class Auth
      * 显示编辑资源表单页.
      *
      * @param  int  $id
-     * @return \think\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -84,9 +114,9 @@ class Auth
     /**
      * 保存更新的资源
      *
-     * @param  \think\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \think\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -97,7 +127,7 @@ class Auth
      * 删除指定资源
      *
      * @param  int  $id
-     * @return \think\Response
+     * @return Response
      */
     public function delete($id)
     {
